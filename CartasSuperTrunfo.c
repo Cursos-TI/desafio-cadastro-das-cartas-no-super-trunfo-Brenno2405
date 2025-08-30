01786fde5da2119a1a5156a13201d43706a32034
Brenno2405 <brennocoutinhocamara@gmail.com>
2025-08-30 22:41:20 +0000

    cartas do Cartas Super Trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
 int main() {
-     
-    char nome[50];
+    
+   char nome[50];
     int população;
     float area;
     float pib;
